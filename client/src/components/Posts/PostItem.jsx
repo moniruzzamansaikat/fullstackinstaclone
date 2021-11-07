957f13eecd91a433d1f26b60758486b0748bd825
Moniruzzman Saikat <saikat.moniruzzman@gmail.com>
2021-11-07 23:05:40 +0600

    fixed reponsesivness

--- client/src/components/Posts/PostItem.jsx
+++ client/src/components/Posts/PostItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import PostImage from "./PostImage";
@@ -26,6 +26,20 @@ function PostItem({ post }) {
     }
   };
 
+  const likeText = useMemo(() => {
+    return post?.likes?.length > 1 ? (
+      <span>
+        liked by{" "}
+        <Link className="custom_link" to={`/profile/${post?.likes[0]._id}`}>
+          {post?.likes[0].name}
+        </Link>{" "}
+        and {post?.likes?.length - 1} others
+      </span>
+    ) : (
+      `${post?.likes?.length} like`
+    );
+  }, [post]);
+
   return (
     <div className="post_item">
       <PostItemHeader post={post} />
@@ -33,9 +47,12 @@ function PostItem({ post }) {
 
       {/* post actions */}
       <div className="post_actions">
-        <PostLike post={post} user={user} />
+        <div className="icons">
+          <PostLike post={post} user={user} />
+          <PostSave post={post} />
+        </div>
 
-        <PostSave post={post} />
+        {likeText}
       </div>
 
       <div style={{ padding: "1rem" }}>
--- client/src/components/Posts/PostLike.jsx
+++ client/src/components/Posts/PostLike.jsx
@@ -1,11 +1,11 @@
 import React, { useCallback, useMemo } from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { useDispatch } from "react-redux";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { FaRegComment } from "react-icons/fa";
 import { dislikePost, likePost } from "../../store/posts/likes";
 
-function PostLike({ post, user, noText }) {
+function PostLike({ post, user }) {
   const dispatch = useDispatch();
   const history = useHistory();
 
@@ -17,38 +17,23 @@ function PostLike({ post, user, noText }) {
     return post.likes.some((like) => like?._id === user?._id);
   }, [post, user]);
 
-  const likeText = useMemo(() => {
-    return post?.likes?.length > 1 ? (
-      <span>
-        liked by{" "}
-        <Link className="custom_link" to={`/profile/${post?.likes[0]._id}`}>
-          {post?.likes[0].name}
-        </Link>{" "}
-        and {post?.likes?.length - 1} others
-      </span>
-    ) : (
-      `${post?.likes?.length} like`
-    );
-  }, [post]);
-
   const goToPost = useCallback(() => {
     history.push(`/posts/${post?._id}`);
   }, [history, post]);
 
   return (
-    <div style={{ display: noText ? "inline" : "flex", alignItems: "center" }}>
+    <div>
       {likedPost ? (
-        <div>
+        <div style={{ display: "flex" }}>
           <AiFillHeart id="icon" onClick={handlePostDislike} />
           <FaRegComment id="icon" onClick={goToPost} />
         </div>
       ) : (
-        <div>
+        <div style={{ display: "flex" }}>
           <AiOutlineHeart id="icon" onClick={handlePostLike} />
           <FaRegComment id="icon" onClick={goToPost} />
         </div>
       )}
-      {!noText && <span>{likeText}</span>}
     </div>
   );
 }
