c604d07eb29c98c3c84e1134abbd5001f9191de8
Moniruzzman Saikat <saikat.moniruzzman@gmail.com>
2021-11-08 10:24:54 +0600

    working on local server

--- client/src/utils/xhr.js
+++ client/src/utils/xhr.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const xhr = Axios.create({
-  baseURL: "/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 /**
@@ -11,7 +11,7 @@ export const xhr = Axios.create({
  */
 export const authRequest = (token) => {
   return Axios.create({
-    baseURL: "/api",
+    baseURL: "http://localhost:5000/api",
     headers: {
       Authorization: `bearer ${token}`,
     },
